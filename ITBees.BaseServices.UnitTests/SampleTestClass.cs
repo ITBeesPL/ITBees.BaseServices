0b856b752916b8d62347fc898f8cc38cb6d2096c
Jakub Zohar Rutkowski <kuba@skb.pl>
2023-02-22 11:06:55 +0100

    Add project files.

---
+++ ITBees.BaseServices.UnitTests/SampleTestClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ITBees.BaseServices.UnitTests
+{
+    public class SampleTestClass
+    {
+        [Test]
+        public void DummyTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ ITBees.BaseServices/Platforms/PlatformSettingsService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using ITBees.Interfaces.Platforms;
+using Microsoft.Extensions.Configuration;
+
+namespace ITBees.Models.Platforms
+{
+    public class PlatformSettingsService : IPlatformSettingsService
+    {
+        private readonly IConfigurationRoot _configurationRoot;
+
+        public PlatformSettingsService(IConfigurationRoot configurationRoot)
+        {
+            _configurationRoot = configurationRoot;
+        }
+
+        public string GetSetting(string key)
+        {
+            var settingValue = _configurationRoot.GetSection(key).Value;
+
+            return settingValue;
+        }
+
+        public List<string> GetPlatformDebugEmails()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Environment GetCurrentEnvironment()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
