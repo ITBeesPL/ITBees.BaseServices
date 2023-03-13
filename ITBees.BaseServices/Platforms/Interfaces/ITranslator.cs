285285fbc3b4d78cc0e3356e7751c547bd1a2b63
Jakub Zohar Rutkowski <kuba@skb.pl>
2023-03-13 16:46:45 +0100

    Base services update

---
+++ ITBees.BaseServices/Platforms/Interfaces/ITranslator.cs
@@ -0,0 +1,19 @@
+﻿using ITBees.Models.Languages;
+
+namespace ITBees.BaseServices.Platforms.Interfaces
+{
+    public interface ITranslator
+    {
+        string Get(Language language, string key);
+        string Get(Language language, LangConst langConst);
+    }
+
+    public interface IUserManagerLangConst
+    {
+        LangConst PrivateCompanyName { get; }
+    }
+    public struct LangConst
+    {
+        public string Value { get; set; }
+    }
+}
